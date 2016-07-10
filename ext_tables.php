8731f7f0b55b1c447ae2176685291d23a1e0ffc9
Nicole Cordes <cordes@cps-it.de>
2016-07-10 22:24:00 +0200

    [FEATURE] Add ExportController and Template

---
+++ ext_tables.php
@@ -0,0 +1,7 @@
+<?php
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['Mask']['modules']['tools_MaskMask']['controllers']['Export'] = [
+    'actions' => [
+        'list',
+    ],
+];
