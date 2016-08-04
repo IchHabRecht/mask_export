a918ba3d2063f6cda3df996d9b1174a4867ac76f
Nicole Cordes <cordes@cps-it.de>
2016-08-04 12:02:26 +0200

    [TASK] Set version to 0.3.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = array (
   'author_email' => 'cordes@cps-it.de',
   'author_company' => 'CPS-IT GmbH',
   'state' => 'beta',
-  'version' => '0.2.0',
+  'version' => '0.3.0-dev',
   'constraints' => 
   array (
     'depends' => 
