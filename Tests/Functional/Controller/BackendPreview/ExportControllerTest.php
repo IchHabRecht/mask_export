a0cce0f7bff1b9282d72ea5fd1d8fc33daf19af9
Nicole Cordes <typo3@cordes.co>
2017-01-28 15:46:38 +0100

    [TASK] Add test for selectbox rendering in backend preview

--- Tests/Functional/Controller/BackendPreview/ExportControllerTest.php
+++ Tests/Functional/Controller/BackendPreview/ExportControllerTest.php
@@ -83,4 +83,20 @@ class ExportControllerTest extends AbstractExportControllerTestCase
             $this->assertArrayHasKey($templatePath, $this->files);
         }
     }
+
+    /**
+     * @test
+     */
+    public function validateFluidTemplateForSelectboxFields()
+    {
+        $this->assertArrayHasKey('Resources/Private/Backend/Templates/Content/Simple-element.html', $this->files);
+        $this->assertContains(
+            '{processedRow.tx_maskexport_simpleselectboxsingle.0} (raw={row.tx_maskexport_simpleselectboxsingle})<br>',
+            $this->files['Resources/Private/Backend/Templates/Content/Simple-element.html']
+        );
+        $this->assertContains(
+            '<f:for each="{processedRow.tx_maskexport_simpleselectboxmulti}" as="item">',
+            $this->files['Resources/Private/Backend/Templates/Content/Simple-element.html']
+        );
+    }
 }
