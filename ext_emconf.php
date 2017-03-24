a5ee1d9371330e99b24e191ea3011dc5a73831d3
Nicole Cordes <typo3@cordes.co>
2017-03-24 09:42:08 +0100

    [TASK] Raise compatibility to TYPO3 8.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '7.6.0-8.5.99',
+      'typo3' => '7.6.0-8.6.99',
       'mask' => '2.0.0-0.0.0',
     ),
     'conflicts' => 
