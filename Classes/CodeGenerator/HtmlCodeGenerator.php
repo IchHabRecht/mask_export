a981ead912a729335b42a6c046fac89cbb9dda7a
Nicole Cordes <typo3@cordes.co>
2018-04-15 17:44:02 +0200

    [TASK] Update license information

--- Classes/CodeGenerator/HtmlCodeGenerator.php
+++ Classes/CodeGenerator/HtmlCodeGenerator.php
@@ -1,29 +1,19 @@
 <?php
 namespace IchHabRecht\MaskExport\CodeGenerator;
 
-/***************************************************************
- *  Copyright notice
+/*
+ * This file is part of the TYPO3 extension mask_export.
  *
- *  (c) 2016 Benjamin Butschell <bb@webprofil.at>, WEBprofil - Gernot Ploiner e.U.
+ * (c) 2016 Benjamin Butschell <bb@webprofil.at>, WEBprofil - Gernot Ploiner e.U.
+ * (c) 2016 Nicole Cordes <typo3@cordes.co>, CPS-IT GmbH
  *
- *  All rights reserved
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
 
 use MASK\Mask\CodeGenerator\AbstractCodeGenerator;
 use MASK\Mask\Helper\FieldHelper;
