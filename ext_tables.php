9ef69ee4c028cf0b088d82e9b1e93366b5b62d3e
Nicole Cordes <typo3@cordes.co>
2020-05-16 13:25:25 +0200

    [FEATURE] Remove original mask elements from TCA

--- ext_tables.php
+++ ext_tables.php
@@ -1,4 +1,7 @@
 <?php
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 defined('TYPO3_MODE') || die();
 
 call_user_func(function () {
@@ -15,5 +18,19 @@ call_user_func(function () {
             'icon' => 'EXT:mask_export/Resources/Public/Icons/Extension.svg',
             'labels' => 'LLL:EXT:mask_export/Resources/Private/Language/locallang_mod.xlf',
         ]
+    );
+
+    $maskElements = array_filter(
+        $GLOBALS['TCA']['tt_content']['types'] ?? [],
+        function ($key) {
+            return strpos((string)$key, 'mask_') === 0;
+        },
+        ARRAY_FILTER_USE_KEY
+    );
+
+    ExtensionManagementUtility::addPageTSConfig(
+        'TCEFORM.tt_content.CType.removeItems := addToList('
+        . implode(',', array_keys($maskElements))
+        . ')'
     );
 });
