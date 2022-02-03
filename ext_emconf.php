df8648246eb292ee5a605e0a6d7c67dc96573c6d
Georg Ringer <georg.ringer@gmail.com>
2022-02-03 13:23:34 +0100

    [TASK] Allow PHP8

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '9.5.0-10.4.99',
+      'typo3' => '9.5.0-11.5.99',
       'mask' => '4.0.0-0.0.0',
     ),
     'conflicts' => 
