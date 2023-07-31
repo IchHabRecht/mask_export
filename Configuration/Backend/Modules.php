7bdd4a7ca08f746222eb456deee9bdc77bc7cbe0
Albrecht Köhnlein <ak@koehnlein.eu>
2023-07-31 17:58:01 +0200

    Update backend module registration

---
+++ Configuration/Backend/Modules.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'mask_export' => [
+        'parent' => 'tools',
+        'position' => ['after' => 'mask_module'],
+        'access' => 'admin',
+        'path' => '/module/mask_export',
+        'icon' => 'EXT:mask_export/Resources/Public/Icons/Extension.svg',
+        'labels' => 'LLL:EXT:mask_export/Resources/Private/Language/locallang_mod.xlf',
+        'extensionName' => 'MaskExport',
+        'controllerActions' => [
+            \IchHabRecht\MaskExport\Controller\ExportController::class => [
+                'list', 'save', 'download', 'install'
+            ]
+        ],
+    ],
+];
--- ext_tables.php
+++ ext_tables.php
@@ -3,21 +3,6 @@
 defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
 
 call_user_func(static function () {
-    TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'IchHabRecht.mask_export',
-        'tools',
-        'mask_export',
-        'after:MaskMask',
-        [
-            \IchHabRecht\MaskExport\Controller\ExportController::class => 'list, save, download, install',
-        ],
-        [
-            'access' => 'admin',
-            'icon' => 'EXT:mask_export/Resources/Public/Icons/Extension.svg',
-            'labels' => 'LLL:EXT:mask_export/Resources/Private/Language/locallang_mod.xlf',
-        ]
-    );
-
     $maskElements = array_filter(
         $GLOBALS['TCA']['tt_content']['types'] ?? [],
         static function ($key) {
