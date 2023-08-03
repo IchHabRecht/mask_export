108f0a22e07874a4741d12c4f852a1d803e37cbe
Albrecht Köhnlein <ak@koehnlein.eu>
2023-08-03 20:56:01 +0200

    Reintroduce compatibility with TYPO3 11

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,8 +23,8 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '9.5.0-12.4.99',
-      'mask' => '4.0.0-8.99.99',
+      'typo3' => '11.5.0-12.4.99',
+      'mask' => '8.0.0-8.99.99',
     ),
     'conflicts' => 
     array (
--- ext_tables.php
+++ ext_tables.php
@@ -3,6 +3,21 @@
 defined('TYPO3') || die();
 
 call_user_func(static function () {
+    TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        'IchHabRecht.mask_export',
+        'tools',
+        'mask_export',
+        'after:MaskMask',
+        [
+            \IchHabRecht\MaskExport\Controller\ExportController::class => 'list, save, download, install',
+        ],
+        [
+            'access' => 'admin',
+            'icon' => 'EXT:mask_export/Resources/Public/Icons/Extension.svg',
+            'labels' => 'LLL:EXT:mask_export/Resources/Private/Language/locallang_mod.xlf',
+        ]
+    );
+
     $maskElements = array_filter(
         $GLOBALS['TCA']['tt_content']['types'] ?? [],
         static function ($key) {
